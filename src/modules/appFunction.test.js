b258214a56fee516535f5ba8b8b0117e6d9e2074
Gracerue <76904512+Gracerue@users.noreply.github.com>
2023-03-09 14:27:06 +0200

    Test editTask, removeAllCompleted and checkTask

--- src/modules/appFunction.test.js
+++ src/modules/appFunction.test.js
@@ -10,6 +10,8 @@ global.window = dom.window;
 const {
   addTask,
   removeTask,
+  EditTask,
+  removeAllCompleted,
   displayTasks,
   tasksArray,
 } = require('./appFunction.js');
@@ -48,3 +50,74 @@ describe('removeTask', () => {
     expect(document.querySelectorAll('.task-li')).toHaveLength(0);
   });
 });
+
+describe('EditTask', () => {
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <ul class="todo-list">
+        <li class="task-li" id="task-0">
+          <input type="text" class="task-input" value="Task 1">
+          <button class="task-btn">
+            <i class="fa-solid fa-ellipsis-vertical task-btn__icon"></i>
+            <i class="fa-solid fa-trash-can task-btn__icon icon-hidden delete-task__button"></i>
+          </button>
+        </li>
+      </ul>
+    `;
+    localStorage.clear();
+    tasksArray.length = 0;
+    tasksArray.push({
+      description: 'Task 1',
+      completed: false,
+      index: 0,
+    });
+    displayTasks();
+  });
+
+  test('should edit task description and save to local storage', () => {
+    const taskInput = document.querySelector('.task-input');
+    taskInput.value = 'Edited task';
+    EditTask(taskInput);
+    expect(tasksArray[0].description).toBe('Edited task');
+  });
+});
+
+describe('remove all complete',() => {
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <ul class="todo-list">
+        <li class="task-li" id="task-0">
+          <input type="checkbox" class="task-checker" checked>
+          <input type="text" class="task-input" value="Task 1">
+          <button class="task-btn">
+            <i class="fa-solid fa-ellipsis-vertical task-btn__icon"></i>
+            <i class="fa-solid fa-trash-can task-btn__icon icon-hidden delete-task__button"></i>
+          </button>
+        </li>
+        <li class="task-li" id="task-1">
+          <input type="checkbox" class="task-checker">
+          <input type="text" class="task-input" value="Task 2">
+          <button class="task-btn">
+            <i class="fa-solid fa-ellipsis-vertical task-btn__icon"></i>
+            <i class="fa-solid fa-trash-can task-btn__icon icon-hidden delete-task__button"></i>
+          </button>
+        </li>
+    </ul>
+  `;
+    localStorage.clear();
+    tasksArray.length = 0;
+    tasksArray.push(
+      {description: 'Task 1', completed: true, index: 0,},
+      {description: 'Task 2', completed: false, index: 1,},
+    );
+    displayTasks();
+  });
+  
+  
+  test('should remove all completed tasks', () => {
+      removeAllCompleted();
+      expect(tasksArray.length).toBe(1);
+      expect(tasksArray[0].description).toBe('Task 2');
+      expect(document.querySelectorAll('.task-li').length).toBe(1);
+  })
+})
